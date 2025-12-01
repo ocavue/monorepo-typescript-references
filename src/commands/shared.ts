aea6c792e5eb8e72ffee9d7b1c1f00c23a97d2b9
ocavue <ocavue@gmail.com>
2025-12-01 14:40:27 +1100

    optional: true

--- src/commands/shared.ts
+++ src/commands/shared.ts
@@ -1,17 +1,18 @@
 import type { FlagParametersForType } from '@stricli/core'
 
 export interface Flags {
-  configName: string[]
-  rootConfigName: string[]
+  configName?: string[]
+  rootConfigName?: string[]
 }
 
-export const flags = {
+export const flags: FlagParametersForType<Flags> = {
   configName: {
     kind: 'parsed',
     parse: String,
     brief:
       'The name of the tsconfig file to update in each package. You can specify multiple file names by repeating the flag. Default: tsconfig.json',
     variadic: true,
+    optional: true,
   },
   rootConfigName: {
     kind: 'parsed',
@@ -19,16 +20,22 @@ export const flags = {
     brief:
       'The name of the tsconfig file to update in the monorepo root. You can specify multiple file names by repeating the flag. Default: tsconfig.json',
     variadic: true,
+    optional: true,
   },
-} satisfies FlagParametersForType<Flags>
+}
 
 // Manually set the default values since Stricli does not support default
 // values for variadic flags. See:
 // https://github.com/bloomberg/stricli/issues/95
-export function withDefaultFlags(flags: Flags): Flags {
-  // prettier-ignore
+export function withDefaultFlags(inputFlags: Flags): Required<Flags> {
   return {
-    configName: flags.configName.length === 0 ? ['tsconfig.json'] : [...flags.configName],
-    rootConfigName: flags.rootConfigName.length === 0 ? ['tsconfig.json'] : [...flags.rootConfigName],
+    configName:
+      inputFlags.configName && inputFlags.configName.length > 0
+        ? inputFlags.configName
+        : ['tsconfig.json'],
+    rootConfigName:
+      inputFlags.rootConfigName && inputFlags.rootConfigName.length > 0
+        ? inputFlags.rootConfigName
+        : ['tsconfig.json'],
   }
 }
